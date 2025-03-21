6a0b567901c77735739a3972b94bafa7efbd2bb2
chasehh523 <chasehh@gmail.com>
2025-03-21 19:44:59 -0400

    game_simulator logic begun to be implemented, but this is pretty difficult and more difficult to code ***CLEANLY*** than we'd imagined

--- handball/game_simulator.py
+++ handball/game_simulator.py
@@ -7,23 +7,67 @@ Date: 1/20/2025 12:32PM PST
 ## For each player, in each team, draw a random contribution to offense and defense
 ## Team A offense - Team B defense -> shots on goal, multiply by goalie save 
 # Pass length correlates with amount of time off clock
+import numpy as np
+
+def simulate_game(home_team, away_team):
+    home_score = 0
+    away_score = 0
+
+    # See game_mechanics.txt
+    home_offense = 0
+    home_defense = 0
+    for player in home_team.starters:
+        home_offense += np.random.normal(player.offense, player.variance)
+        home_defense += np.random.normal(player.defense, player.variance)
+    for player in home_team.bench:
+        home_offense += np.random.normal(player.offense, player.variance)
+        home_defense += np.random.normal(player.defense, player.variance)
+    home_ratio = home_offense / (home_offense + away_defense)
+    home_goalie = np.random.normal(home_team.goalie.goalie_score, home_team.goalie.variance)
+    
+    away_offense = 0
+    away_defense = 0
+    for player in away_team.starters:
+        away_offense += np.random.normal(player.offense, player.variance)
+        away_defense += np.random.normal(player.defense, player.variance)
+    for player in away_team.bench:
+        away_offense += np.random.normal(player.offense, player.variance)
+        away_defense += np.random.normal(player.defense, player.variance)
+    away_ratio = away_offense / (away_offense + home_defense)
+    away_goalie = np.random.normal(away_team.goalie.goalie_score, away_team.goalie.variance)
+
+    has_posession = 1
+
+    ball_at = 20
 
 
-def simulate_game(home_team, visiting_team):
+    prob = np.random.rand(1, 1000)
+    i = 0
+    k = 0.5 ######### TO TUNE
 
-    # Calculate real-game stats (random number generated from gaussian distribution)
-
-    # Calculate overall team stats
-
-    # With the game-specific team stats, randomize the winner of the game 
-
-    # Calculate game score
-
-    # Attribute the goals scored to individual players
+    while True:
+        
+        if np.random.uniform(0,1) < home_ratio: #Is a pass successful?
+            
+            if np.random.uniform(0,1) < odds_of_taking_shot(ball_at): #If pass would be successful, was the pass a shot?
+                
+                if np.random.uniform(0,1) < np.e ^ (-k * (40-ball_at)): #If shot was taken, was it on goal?
+                    #if shot was on goal, was a goal scored?
+                    if np.random.uniform(0,1) < home_offense / (home_offense + away_defense + away_goalie): #if shot was on goal, was a goal scored?
+                        home_score +=1 #goal scored
+                        break
+                    elif np.random.uniform(0,1) < 0.1: pass #offensive recovery
+                    else: break #turnover
+                elif np.random.uniform(0,1) < 0.1: pass #offensive recovery
+                else: break #turnover
+            else: ball_at += np.random.normal(4, 1.5) #normal pass completed and advanced
+        else: break #turnover
 
     raise NotImplementedError
 
-
+def odds_of_taking_shot(yard):
+    return 1 / (1 + np.e ^ (-0.3 * (yard-34)))
+    
 
 def generate_game_analysis(home_team, visiting_team, individual_performances, results_dump):
 
